5841906b35c338f9dc0487e2235ad7a8aff669a5
akinloludavid <67186221+akinloludavid@users.noreply.github.com>
2021-02-28 12:30:13 -0700

    Sidehustle form handling assigment

---
+++ form/welcome.php
@@ -0,0 +1,11 @@
+
+
+<?php 
+echo '<h3>Your information: </h3>';
+echo 'Your name: ';
+echo $_POST['name'].'<br>';
+echo 'Your email: ';
+echo $_POST['email'].'<br>';
+echo 'Your tech stack: ';
+echo $_POST['stack'].'<br>';
+?>
